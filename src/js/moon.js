b95439f3d5979ad375d9267556ecfa93d4ed39b7
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2025-01-18 01:36:32 -0500

    light direction not working

--- src/js/moon.js
+++ src/js/moon.js
@@ -2,21 +2,22 @@ import { Moon, Earth, Sun } from "./utils.js";
 
 export const moonRotate = (p) => {
     let moon;
-    let earth;
-    let sun;
+    // let earth;
+    // let sun;
 
     p.setup = () => {
         p.createCanvas(800, 400, p.WEBGL);
         p.noStroke();
+        p.frameRate(10);
 
-        let moonPos = p.createVector(100, 0, 0);
-        moon = new Moon(p, moonPos, 50, 0);
+        let moonPos = p.createVector(0, 0, 0);
+        moon = new Moon(p, moonPos, 100, 0);
 
-        let earthPos = p.createVector(-100, 0, 0);
-        earth = new Earth(p, earthPos, 100, 0);
+        // let earthPos = p.createVector(-100, 0, 0);
+        // earth = new Earth(p, earthPos, 100, 0);
 
-        let sunPos = p.createVector(0, 0, -500);
-        sun = new Sun(p, sunPos, 300, 0);
+        // let sunPos = p.createVector(0, 0, -500);
+        // sun = new Sun(p, sunPos, 300, 0);
     };
 
     p.draw = () => {
@@ -24,11 +25,11 @@ export const moonRotate = (p) => {
         p.randomSeed(1);
 
         moon.draw();
-        earth.draw();
-        sun.draw();
-        moon.rotate(0.2);
-        earth.rotate(0.1);
-        sun.rotate(0.01);
+        // earth.draw();
+        // sun.draw();
+        moon.rotate(1);
+        // earth.rotate(0.1);
+        // sun.rotate(0.01);
 
         p.orbitControl(2);
     };
--- src/js/utils.js
+++ src/js/utils.js
@@ -5,35 +5,42 @@ class Body {
      * @param {object} p - The p5 instance
      * @param {p5.Vector} pos - Location of the body
      * @param {number} r - Radius of the body
-     * @param {number} rot - Initial rotation angle (in radians). 0 = dark side towards camera.
-     * @param {number} res - Resolution of the render
+     * @param {number} rot - Rotation angle (in radians)
+     * @param {number} res - Resolution of the render (2pi/res)
+     * @param {number} light - Angle from which the light is coming
      */
-    constructor(p, pos, r, rot, res=0.2) {
+    constructor(p, pos, r, rot, res=80, light=0) {
         this.p = p;
         this.pos = pos;
         this.r = r;
         this.color = p.color(255);
         this.rot = rot;
-        this.res = res;
+        this.res = p.TWO_PI/res;
+        // Default light comes from the direction of sun (pos X)
+        this.light = light;
     }
 
     instanceVars() {
-        return [this.p, this.pos, this.r, this.color, this.rot, this.res];
+        return [
+            this.p, this.pos, this.r, this.color,
+            this.rot, this.res, this.light
+        ];
     }
 
     draw() {
-        const [p, pos, r, c, rot, res] = this.instanceVars();
+        const [p, pos, r, c, rot, res, light] = this.instanceVars();
         p.push();
         p.translate(pos);
         p.rotateY(-rot);
-        drawHemisphere(p, r, c, res, true);
-        drawHemisphere(p, r, c, res, false);
+        drawLitSphere(p, r, c, rot, res, light);
+        // drawHemisphere(p, r, c, res, true);
+        // drawHemisphere(p, r, c, res, false);
         p.pop();
     }
 
     // Rotates moon by given angle
-    rotate(rotAngle) {
-        this.rot += rotAngle;
+    rotate(resAmt) {
+        this.rot = (this.rot + resAmt * this.res) % this.p.TWO_PI;
     }
 }
 
@@ -85,7 +92,88 @@ function noisyColor(p, color, grain) {
     );
 }
 
+function isLight(p, lon, light) {
+    lon %= p.TWO_PI;
+    return lon < light + p.PI/2 || lon > light + p.PI*3/2;
+}
+
+function drawLitSphere(p, r, baseColor, rot, res, light) {
+    p.beginShape(p.TRIANGLES);
+    for (let lon = rot; lon < rot+p.TWO_PI; lon += res) {
+        let lightStrip = isLight(p, lon, light);
+        let c = lightStrip ? baseColor : darkenColor(p, baseColor);
+        for (let lat = -p.PI/2; lat < p.PI/2; lat += res) {
+            let x1 = p.cos(lat) * p.cos(lon) * r;
+            let y1 = p.sin(lat) * r;
+            let z1 = p.cos(lat) * p.sin(lon) * r;
+
+            let x2 = p.cos(lat + res) * p.cos(lon) * r;
+            let y2 = p.sin(lat + res) * r;
+            let z2 = p.cos(lat + res) * p.sin(lon) * r;
+
+            let x3 = p.cos(lat) * p.cos(lon + res) * r;
+            let y3 = p.sin(lat) * r;
+            let z3 = p.cos(lat) * p.sin(lon + res) * r;
+
+            let x4 = p.cos(lat + res) * p.cos(lon + res) * r;
+            let y4 = p.sin(lat + res) * r;
+            let z4 = p.cos(lat + res) * p.sin(lon + res) * r;
+
+            // let isLight = lon > light - p.PI/2 && lon <= light + p.PI/2;
+            p.fill(noisyColor(p, c, 5));
+
+            // Connect adjacent points to form triangles
+            p.vertex(x1, y1, z1);
+            p.vertex(x2, y2, z2);
+            p.vertex(x3, y3, z3);
+
+            p.vertex(x3, y3, z3);
+            p.vertex(x2, y2, z2);
+            p.vertex(x4, y4, z4);
+        }
+    }
+    p.endShape();
+}
+
 function drawHemisphere(p, r, baseColor, res, isDark) {
+    let start = isDark ? p.PI/2 : -p.PI/2;
+    let end = isDark ? p.PI*3/2 : p.PI/2;
+    p.beginShape(p.TRIANGLES);
+    for (let lon = start; lon < end; lon += res) {
+        for (let lat = -p.PI/2; lat < p.PI/2; lat += res) {
+            let x1 = p.cos(lat) * p.cos(lon) * r;
+            let y1 = p.sin(lat) * r;
+            let z1 = p.cos(lat) * p.sin(lon) * r;
+
+            let x2 = p.cos(lat + res) * p.cos(lon) * r;
+            let y2 = p.sin(lat + res) * r;
+            let z2 = p.cos(lat + res) * p.sin(lon) * r;
+
+            let x3 = p.cos(lat) * p.cos(lon + res) * r;
+            let y3 = p.sin(lat) * r;
+            let z3 = p.cos(lat) * p.sin(lon + res) * r;
+
+            let x4 = p.cos(lat + res) * p.cos(lon + res) * r;
+            let y4 = p.sin(lat + res) * r;
+            let z4 = p.cos(lat + res) * p.sin(lon + res) * r;
+
+            let c = isDark ? darkenColor(p, baseColor) : baseColor;
+            p.fill(noisyColor(p, c, 5));
+
+            // Connect adjacent points to form triangles
+            p.vertex(x1, y1, z1);
+            p.vertex(x2, y2, z2);
+            p.vertex(x3, y3, z3);
+
+            p.vertex(x3, y3, z3);
+            p.vertex(x2, y2, z2);
+            p.vertex(x4, y4, z4);
+        }
+    }
+    p.endShape();
+}
+
+function drawHemisphere2(p, r, baseColor, res, isDark) {
     let start = isDark ? 0 : p.PI;
     let end = isDark ? p.PI/2 : p.PI*3/2;
     p.beginShape(p.TRIANGLES);
