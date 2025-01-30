8961794009cd610f27068239b88fa2ec1826cc1c
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2025-01-30 11:58:31 -0500

    wrangling vite

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "./",
   publicDir: "public",
-  base: "/moon/",
+  base: process.env.NODE_ENV === 'production' ? '/moon/' : '/',
   build: {
     outDir: "dist",
+    assetsDir: "assets",
     rollupOptions: {
       // Prevent Vite from injecting scripts into <head>
       input: "index.html"
