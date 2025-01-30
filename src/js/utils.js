b120dba21b41928e8d6b8ffd398448feac52213d
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2025-01-30 13:21:11 -0500

    split long line

--- src/js/utils.js
+++ src/js/utils.js
@@ -213,7 +213,9 @@ export class Font {
     loadFonts() {
         const [p, fam] = [this.p, this.fam];
         let ext = this.isTTF ? ".ttf" : ".otf";
-        let prefix = import.meta.env.NODE_ENV === "production" && import.meta.env.MODE === "production"
+        let nodeEnv = import.meta.env.NODE_ENV;
+        let mode = import.meta.env.MODE;
+        let prefix = nodeEnv === "production" && mode === "production"
             ? "/moon" : "";
         let regular = p.loadFont(`${prefix}/assets/${fam}/${fam}-Regular${ext}`);
         let bold = p.loadFont(`${prefix}/assets/${fam}/${fam}-Bold${ext}`);
