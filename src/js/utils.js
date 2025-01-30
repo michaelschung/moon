a38d402005857912ae58e262f2637f004ca184bb
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2025-01-30 15:01:21 -0500

    use VITE_BASE_PATH in JS

--- src/js/utils.js
+++ src/js/utils.js
@@ -214,11 +214,11 @@ export class Font {
         const [p, fam] = [this.p, this.fam];
         let ext = this.isTTF ? ".ttf" : ".otf";
         let base = import.meta.env.VITE_BASE_PATH;
-        console.log(base);
+        // console.log(base);
         // let prefix = nodeEnv === "local" ? "" : (mode === "production" ? "/moon" : "");
         // let prefix = nodeEnv === "production" ? "/moon" : "";
         let ci = import.meta.env.CI;
-        let prefix = ci ? "/moon" : "";
+        let prefix = base || "";
         let regular = p.loadFont(`${prefix}/assets/${fam}/${fam}-Regular${ext}`);
         let bold = p.loadFont(`${prefix}/assets/${fam}/${fam}-Bold${ext}`);
         let italic = p.loadFont(`${prefix}/assets/${fam}/${fam}-Italic${ext}`);
