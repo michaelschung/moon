2b1f16c13dc21917193c967f01e9b254bffce549
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2025-02-04 22:18:13 -0500

    start Orbit class, but stuck

---
+++ src/three/Orbit.jsx
@@ -0,0 +1,14 @@
+import { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useTexture } from "@react-three/drei";
+
+export function Orbit({primary, satellite, r, tilt}) {
+    const orbitRef = useRef();
+    const revAngle = useRef(0);
+
+    return (
+        <>
+            {/*  */}
+        </>
+    );
+}
