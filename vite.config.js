d2d1346519dc5531a2efc4294bc0bf51f635c9c8
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2025-02-13 13:42:48 -0500

    remove assetsBase

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // root: "./",
-  root: process.env.VITE_BASE_PATH || "./",
+  root: "./",
   publicDir: "public",
   // base: process.env.NODE_ENV === "production" ? "/moon/" : "/",
   base: process.env.VITE_BASE_PATH || "/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
-    assetsBase: "/",
     rollupOptions: {
       // Prevent Vite from injecting scripts into <head>
       input: "index.html"
